576510b29343d38459d18f0b448d666e51d4a23b
Khalifa Ahmad <ahmadkhalifa484@gmail.com>
2023-11-20 20:32:36 +0100

    added semantics some meta data and ogp. also Header, Nav, Main and Footer components.

---
+++ src/components/Footer.js
@@ -0,0 +1,59 @@
+
+
+function Footer() {
+  return (
+    <footer>
+
+      <section>
+        <img src="" alt=""></img>
+      </section>
+
+      <article>
+        <section>
+          <h2>Doormant Navigation</h2>
+        </section>
+
+        <section>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Reservations</a></li>
+            <li><a href="">Order Online</a></li>
+            <li><a href="">Login</a></li>
+          </ul>
+        </section>
+      </article>
+
+      <article>
+        <section>
+          <h2>Contact</h2>
+        </section>
+
+        <section>
+          <ul>
+            <li><a>Address</a></li>
+            <li><a>Phone Number</a></li>
+            <li><a>Email</a></li>
+          </ul>
+        </section>
+      </article>
+
+      <article>
+        <section>
+          <h2>Social Media Links</h2>
+        </section>
+
+        <section>
+          <ul>
+            <li><a>Facebook</a></li>
+            <li><a>Instagram</a></li>
+          </ul>
+        </section>
+      </article>
+
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+
+
+function Main() {
+  return (
+    <main>
+      <section>Hero</section>
+      <section>Todays specials</section>
+      <section>Testimonials</section>
+      <section>About</section>
+    </main>
+  )
+}
+
+export default Main;
